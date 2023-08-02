7d1629f3a34821bdc21628d50fbe2034a656c7dd
tamagoez <87574268+tamagoez@users.noreply.github.com>
2023-08-02 16:59:32 +0000

    add temp fix

--- utils/db/login.ts
+++ utils/db/login.ts
@@ -1,6 +1,6 @@
 export async function databaseAuthLogin(email: string, password: string) {
-  const { data, error } = await supabase.auth.signInWithPassword({
-    email: "example@email.com",
-    password: "example-password",
-  });
+  // const { data, error } = await supabase.auth.signInWithPassword({
+  //   email: "example@email.com",
+  //   password: "example-password",
+  // });
 }
