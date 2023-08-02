030cc636e012ac8a8ebaca4a5319d62cf23eae49
tamagoez <87574268+tamagoez@users.noreply.github.com>
2023-08-02 16:57:02 +0000

    uninstall chekra-ui...

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,17 +1,7 @@
-import { Box, Button, Input } from "@chakra-ui/react";
-
-export default function Login() {
+export default function Auth() {
   return (
     <>
-      <Box>
-        <Button colorScheme="teal" variant="outline">
-          Login with neemBase
-        </Button>
-        <Input variant="flushed" placeholder="Flushed" />
-        <Input variant="flushed" placeholder="Flushed" />
-        <Button>Login</Button>
-        <Button>Signup</Button>
-      </Box>
+      <p>すべて消滅したログインページ</p>
     </>
   );
 }
