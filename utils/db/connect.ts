0e4c2df1c353aab7cb7fb9842796d6ed264bdaa1
tamagoez <87574268+tamagoez@users.noreply.github.com>
2023-08-03 17:30:18 +0000

    strict

--- utils/db/connect.ts
+++ utils/db/connect.ts
@@ -3,4 +3,4 @@ import { createClient } from "@supabase/supabase-js";
 const baseSupabaseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 const baseSupabaseKey = process.env.NEXT_PUBLIC_BASE_KEY;
 
-export default createClient(baseSupabaseUrl, baseSupabaseKey);
+export default createClient(baseSupabaseUrl!, baseSupabaseKey!);
