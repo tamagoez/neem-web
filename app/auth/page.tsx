d0ef2c3b6b7ad9ea9d4b8d01028870cb9682e4a1
tamagoez <87574268+tamagoez@users.noreply.github.com>
2023-08-03 17:14:30 +0000

    add auth

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,7 +1,69 @@
+"use client";
+
+import {
+  Container,
+  FormControl,
+  FormLabel,
+  Input,
+  VStack,
+  HStack,
+  FormHelperText,
+  FormErrorMessage,
+  Button,
+} from "../common/chakra-ui";
+import { useState } from "react";
+
 export default function Auth() {
+  const [authmode, setAuthmode] = useState<"login" | "signup" | undefined>(
+    undefined
+  );
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const isEmailError =
+    /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+      email
+    );
+  const isPasswordError = password === "";
+
   return (
     <>
-      <p>すべて消滅したログインページ</p>
+      <VStack>
+        <Container
+          maxW="md"
+          borderColor="gray"
+          borderWidth="1px"
+          dropShadow="sm"
+        >
+          <FormControl isInvalid={isEmailError}>
+            <FormLabel textColor="black">Email</FormLabel>
+            <Input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            {!isEmailError ? (
+              <></>
+            ) : (
+              <FormErrorMessage>メールを入力してください</FormErrorMessage>
+            )}
+          </FormControl>
+          <FormControl isInvalid={isPasswordError}>
+            <FormLabel textColor="black">Password</FormLabel>
+            <Input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            {!isPasswordError ? (
+              <></>
+            ) : (
+              <FormErrorMessage>パスワードを入力してください</FormErrorMessage>
+            )}
+          </FormControl>
+          <Button>ログイン</Button>
+          <Button>アカウント作成・連携</Button>
+        </Container>
+      </VStack>
     </>
   );
 }
---
+++ app/home/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <p>タイムラインになる予定</p>;
+}
