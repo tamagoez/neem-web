710fbeacdc456e9ad5f76cac7472ddfe83797b48
tamagoez <87574268+tamagoez@users.noreply.github.com>
2023-08-19 16:01:21 +0000

    crypto?

--- utils/generate.ts
+++ utils/generate.ts
@@ -1,7 +1,9 @@
+import crypto from "crypto";
+
 export const generateRandomString = (n: number): string => {
-  const S = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  const S = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
   return Array.from(crypto.getRandomValues(new Uint32Array(n)))
     .map((v) => S[v % S.length])
-    .join('');
+    .join("");
 };
