99ef958fb3ef02415ff181e0ce405214c152e043
tamagoez <87574268+tamagoez@users.noreply.github.com>
2023-09-01 16:22:01 +0000

    ?????????????????????????????

--- app/auth/issue.ts
+++ app/auth/issue.ts
@@ -3,7 +3,7 @@
 // import { issueSignup } from "../../scripts/base/issue";
 
 import { fetchBase } from "../../utils/base/sender";
-import { adminDBInsert } from "../../utils/db/admincontrol";
+// import { adminDBInsert } from "../../utils/db/admincontrol";
 // import { generateRandomString } from "../../utils/generate";
 
 export async function issueSignupServer() {
@@ -11,7 +11,7 @@ export async function issueSignupServer() {
   try {
     // 生成
     const data = await fetchBase("signup", {});
-    await adminDBInsert("signup_issue", { token: data.token }, undefined);
+    // await adminDBInsert("signup_issue", { token: data.token }, undefined);
     return data;
   } catch (error: any) {
     throw new Error(error);
