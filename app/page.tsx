b604162a6d5c7909b410bf2e31bfadecab35eb9d
tamagoez <87574268+tamagoez@users.noreply.github.com>
2023-10-30 16:44:57 +0000

    update index

--- app/page.tsx
+++ app/page.tsx
@@ -8,22 +8,15 @@ const IndexPage = () => (
   <>
     <div className={styles.title_thumb}>
       <div className={styles.title_text}>
-        <p className={novafont.className} style={{ fontSize: "20vw" }}>
+        <p className={novafont.className} style={{ fontSize: "250px" }}>
           neem
         </p>
         <br />
         <p
           className={novafont.className}
-          style={{ fontSize: "4vw", marginTop: "-5vw" }}
+          style={{ fontSize: "3vw", marginTop: "-5vw" }}
         >
-          Still Developing!
-        </p>
-        <br />
-        <p
-          className={novafont.className}
-          style={{ fontSize: "4vw", marginTop: "-2vw" }}
-        >
-          See you soon
+          Now Developing! See you soon!
         </p>
       </div>
     </div>
